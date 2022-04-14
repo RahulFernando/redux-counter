9d68f29857485b0cf3c257d8b3f9047db4bd1134
RahulFernando <rahulrulz680@gmail.com>
2022-04-14 19:30:52 +0530

    show counter

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,25 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const init = { counter: 0, showCounter: true }
+
+const counterReducer = (state = init, action) => {
     if (action.type === 'INCREMENT') {
         return {
             counter: action.value ? state.counter + action.value : state.counter + 1,
+            showCounter: state.showCounter
         }
     }
     if (action.type === 'DECREMENT') {
         return {
             counter: state.counter - 1,
+            showCounter: state.showCounter
+        }
+    }
+
+    if (action.type === 'TOGGLE') {
+        return {
+            counter: state.counter,
+            showCounter: !state.showCounter
         }
     }
 
